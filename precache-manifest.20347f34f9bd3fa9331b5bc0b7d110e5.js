1dc86d0b7cd99f5460cd72f59b9eb218bae141cf
MaryB <marybmichaels@outlook.com>
2019-05-27 01:23:04 -0500

    Updates

---
+++ precache-manifest.20347f34f9bd3fa9331b5bc0b7d110e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff340cb5fcf8bb3cd10b59fb3fc123c",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "56a80047c87638826e40",
+    "url": "/react-clicky-game/static/css/main.94039f23.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "56a80047c87638826e40",
+    "url": "/react-clicky-game/static/js/main.8d28c5ec.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  }
+]);
